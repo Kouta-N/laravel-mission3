c956d7ccb0ccb1176afb10c5f33c08761516996a
kakashi <nk29time@gmail.com>
2021-08-11 22:46:59 +0900

    first

--- mission3/routes/web.php
+++ mission3/routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TaskController@index')->name('tasks.index');
+Route::resource('/tasks', 'TaskController')->except(['index']);
